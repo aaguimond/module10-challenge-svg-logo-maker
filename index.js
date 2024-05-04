858ddc37bab3be429658377945874733f7b67136
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-05-03 22:13:28 -0500

    we have a viable working product! Need to work on testing suites next

--- index.js
+++ index.js
@@ -1,3 +1,257 @@
 const fs = require('fs')
-const shapes = require('./lib/shapes.js')
+const { Square, Rectangle, Circle, Triangle, InvertedTriangle, Pentagon, Hexagon } = require('./lib/shapes.js')
+const inquirer = require('inquirer')
 
+const shapeChoices = [
+    { name: "Square", value: "Square" },
+    { name: "Rectangle", value: "Rectangle" },
+    { name: "Circle", value: "Circle" },
+    { name: "Triangle", value: "Triangle" },
+    { name: "Inverted Triangle", value: "InvertedTriangle" },
+    { name: "Pentagon", value: "Pentagon" },
+    { name: "Hexagon", value: "Hexagon" }
+]
+
+function validColor(value) {
+    const hexRegex = /^#(?:[0-9a-fA-F]{3}){1,2}$/;
+    const colorKeywords = [
+        `aliceblue`,
+        `antiquewhite`,
+        `aqua`,
+        `aquamarine`,
+        `azure`,
+        `beige`,
+        `bisque`,
+        `black`,
+        `blanchedalmond`,
+        `blue`,
+        `blueviolet`,
+        `brown`,
+        `burlywood`,
+        `cadetblue`,
+        `chartreuse`,
+        `chocolate`,
+        `coral`,
+        `cornflowerblue`,
+        `cornsilk`,
+        `crimson`,
+        `cyan`,
+        `darkblue`,
+        `darkcyan`,
+        `darkgoldenrod`,
+        `darkgray`,
+        `darkgrey`,
+        `darkgreen`,
+        `darkkhaki`,
+        `darkmagenta`,
+        `darkolivegreen`,
+        `darkorange`,
+        `darkorchid`,
+        `darkred`,
+        `darksalmon`,
+        `darkseagreen`,
+        `darkslateblue`,
+        `darkslategray`,
+        `darkslategrey`,
+        `darkturquoise`,
+        `darkviolet`,
+        `deeppink`,
+        `deepskyblue`,
+        `dimgray`,
+        `dimgrey`,
+        `dodgerblue`,
+        `firebrick`,
+        `floralwhite`,
+        `forestgreen`,
+        `fuchsia`,
+        `gainsboro`,
+        `ghostwhite`,
+        `gold`,
+        `goldenrod`,
+        `gray`,
+        `grey`,
+        `green`,
+        `greenyellow`,
+        `honeydew`,
+        `hotpink`,
+        `indianred`,
+        `indigo`,
+        `ivory`,
+        `khaki`,
+        `lavender`,
+        `lavenderblush`,
+        `lawngreen`,
+        `lemonchiffon`,
+        `lightblue`,
+        `lightcoral`,
+        `lightcyan`,
+        `lightgoldenrodyellow`,
+        `lightgray`,
+        `lightgrey`,
+        `lightgreen`,
+        `lightpink`,
+        `lightsalmon`,
+        `lightseagreen`,
+        `lightskyblue`,
+        `lightslategray`,
+        `lightslategrey`,
+        `lightsteelblue`,
+        `lightyellow`,
+        `lime`,
+        `limegreen`,
+        `linen`,
+        `magenta`,
+        `maroon`,
+        `mediumaquamarine`,
+        `mediumblue`,
+        `mediumorchid`,
+        `mediumpurple`,
+        `mediumseagreen`,
+        `mediumslateblue`,
+        `mediumspringgreen`,
+        `mediumturquoise`,
+        `mediumvioletred`,
+        `midnightblue`,
+        `mintcream`,
+        `mistyrose`,
+        `moccasin`,
+        `navajowhite`,
+        `navy`,
+        `oldlace`,
+        `olive`,
+        `olivedrab`,
+        `orange`,
+        `orangeRed`,
+        `orchid`,
+        `palegoldenRod`,
+        `palegreen`,
+        `paleturquoise`,
+        `palevioletred`,
+        `papayawhip`,
+        `peachpuff`,
+        `peru`,
+        `pink`,
+        `plum`,
+        `powderblue`,
+        `purple`,
+        `red`,
+        `rosybrown`,
+        `royalblue`,
+        `saddlebrown`,
+        `salmon`,
+        `sandybrown`,
+        `seagreen`,
+        `seashell`,
+        `sienna`,
+        `silver`,
+        `skyblue`,
+        `slateblue`,
+        `slategray`,
+        `slategrey`,
+        `snow`,
+        `springgreen`,
+        `steelblue`,
+        `tan`,
+        `teal`,
+        `thistle`,
+        `tomato`,
+        `turquoise`,
+        `violet`,
+        `wheat`,
+        `white`,
+        `whitesmoke`,
+        `yellow`,
+        `yellowgreen`,
+      ]
+    return hexRegex.test(value) || colorKeywords.includes(value.toLowerCase());
+}
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter between one and three characters for your logo.',
+        validate: input => input.length >= 1 && input.length <= 3 ? true : 'Please enter one to three characters.'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Please enter a color or a hexadecimal color code for your text.',
+        filter: textColor => textColor.trim(),
+        validate: color => validColor(color) ? true : 'Please enter a valid color or hexadecimal color code.'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Which shape would you like for your logo?', 
+        choices: shapeChoices
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Please enter a color or a hexadecimal color code for your shape.',
+        filter: shapeColor => shapeColor.trim(),
+        validate: color => validColor(color) ? true : 'Please enter a valid color or hexadecimal color code.'
+    },
+    {
+        type: 'confirm',
+        name: 'outline',
+        message: 'Would you like an outline for the shape in your logo?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'outlineColor',
+        message: 'What color would you like for the outline?',
+        when: function(answers) {
+            return answers.outline;
+        },
+        filter: outlineColor => outlineColor.trim()
+    }
+];
+
+function writeToFile(filename, response) {
+    fs.writeFile(`./examples/${filename}`, response, (error) => {
+        if (error) {
+            console.error('There was an error creating the SVG file: ', error);
+        } else {
+            console.log('SVG file was successfully created');
+        }
+    });
+}
+
+function createShape(response) {
+    const { shape, shapeColor, text, textColor, stroke } = response;
+    const shapeClassMap = {
+        Square: Square,
+        Rectangle: Rectangle,
+        Circle: Circle,
+        Triangle: Triangle,
+        InvertedTriangle: InvertedTriangle,
+        Pentagon: Pentagon,
+        Hexagon: Hexagon
+    };
+
+    const ShapeClass = shapeClassMap[shape];
+    if (!ShapeClass) {
+        throw new Error('Invalid shape selected')
+    }
+
+    return new ShapeClass(shapeColor, response.outlineColor || 'none', text, textColor)
+}
+
+// Initializing our application
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            console.log('Answers: ', response);
+            const shapeInstance = createShape(response);
+            const svgContent = shapeInstance.toSVG();
+            writeToFile(`${response.text}_logo.svg`, svgContent);
+        })
+        .catch((error) => {
+            console.error('An error occurred: ', error);
+        });
+}
+
+init();
